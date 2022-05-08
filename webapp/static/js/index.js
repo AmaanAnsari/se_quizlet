7baaf6f1a7d47fdde7efc8008a6ee7f001a8cace
RobinBxff <xGit220x>
2022-05-08 13:17:42 +0200

    Landing Page Quizzes

--- webapp/static/js/index.js
+++ webapp/static/js/index.js
@@ -26,7 +26,7 @@ template: `
                         <h2 class="text-white mt-0">We've got what you need!</h2>
                         <hr class="divider divider-light" />
                         <p class="text-white-75 mb-4">We've got the best quizzes out there!</p>
-                        <a class="btn btn-light btn-xl" href="pages/quiz.html">Try it out!</a>
+                        <a class="btn btn-light btn-xl" href="pages/quiz_overview.html">Try it out!</a>
                     </div>
                 </div>
             </div>
@@ -74,66 +74,66 @@ template: `
 `
 })
 
-Vue.component('references', {
-template: `
-<div id="portfolio">
-            <div class="container-fluid p-0">
-                <div class="row g-0">
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
-                            <div class="portfolio-box-caption p-3">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-`})
+// Vue.component('references', {
+// template: `
+// <div id="portfolio">
+//             <div class="container-fluid p-0">
+//                 <div class="row g-0">
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
+//                             <div class="portfolio-box-caption">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="..." />
+//                             <div class="portfolio-box-caption">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
+//                             <div class="portfolio-box-caption">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
+//                             <div class="portfolio-box-caption">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
+//                             <div class="portfolio-box-caption">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="col-lg-4 col-sm-6">
+//                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="Project Name">
+//                             <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
+//                             <div class="portfolio-box-caption p-3">
+//                                 <div class="project-category text-white-50">Category</div>
+//                                 <div class="project-name">Project Name</div>
+//                             </div>
+//                         </a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+// `})
